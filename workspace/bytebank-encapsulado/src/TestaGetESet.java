628dfc3d871a0793b5c70baf6ddedb2f28b96ee8
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-03 10:07:28 -0300

    Committing "Java OO" sixth lesson

--- workspace/bytebank-encapsulado/src/TestaGetESet.java
+++ workspace/bytebank-encapsulado/src/TestaGetESet.java
@@ -2,9 +2,8 @@
 public class TestaGetESet {
 
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		//conta.numero = 1337;
-		conta.setNumero(1337);
+		Conta conta = new Conta(1337, 24226);
+
 		System.out.println(conta.getNumero());
 		
 		Cliente paulo = new Cliente();
@@ -15,13 +14,10 @@ public class TestaGetESet {
 		
 		System.out.println(conta.getTitular().getNome());
 		
-		
-		
 		conta.getTitular().setProfissao("programador");
 		// agora em duas linhas:
 		Cliente titularDaConta = conta.getTitular();
 		titularDaConta.setProfissao("programador");
-		
 		
 		System.out.println(titularDaConta);
 		System.out.println(paulo);
