a456841a5917de59a7d42e76976a34347e85f228
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-03 17:41:15 -0300

    Committing "Java Polimorfismo" seventh lesson

--- workspace/bytebank-herdado-conta/src/ContaCorrente.java
+++ workspace/bytebank-herdado-conta/src/ContaCorrente.java
@@ -1,7 +1,7 @@
 
 
 //new ContaCorrente()
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
@@ -18,6 +18,12 @@ public class ContaCorrente extends Conta {
 	@Override
 	public void deposita(double valor) {
 		super.saldo += valor;
+	}
+
+
+	@Override
+	public double getValorImposto() {
+		return super.saldo * 0.01;
 	}
 
 }
