7b271bd6f3475fbd0b7889f6d02eeae34eb4d54a
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-03 15:39:09 -0300

    Committing "Java Polimorfismo" third lesson

---
+++ workspace/bytebank-herdado/src/EditorVideo.java
@@ -0,0 +1,9 @@
+
+public class EditorVideo extends Funcionario {
+	
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do Editor de video");
+		return super.getBonificacao() + 100;
+	}
+	
+}
--- workspace/bytebank-herdado/src/Gerente.java
+++ workspace/bytebank-herdado/src/Gerente.java
@@ -16,6 +16,7 @@ public class Gerente extends Funcionario {
 	}
 	
 	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do GERENTE");
 		return super.getBonificacao() + super.getSalario();
 	}
 	
