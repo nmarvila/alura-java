4126cd56aaafa382355dc6e60ff5fb413cbc4efc
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-10 21:03:03 -0300

    Committing "Java e java.util" fifth lesson

--- workspace/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteArrayList.java
+++ workspace/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteArrayList.java
@@ -1,8 +1,10 @@
 package br.com.bytebank.banco.test.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Vector;
 
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
@@ -12,7 +14,7 @@ public class TesteArrayList {
 	public static void main(String[] args) {
 
 		//Generics
-		List<Conta> lista = new ArrayList<Conta>();
+		List<Conta> lista = new ArrayList<Conta>();//thread safe
 		
 		Conta cc = new ContaCorrente(22, 11);
 		lista.add(cc);
