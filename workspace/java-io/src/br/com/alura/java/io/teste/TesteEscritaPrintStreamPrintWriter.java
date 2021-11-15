38fc84f2628d3aa0fe5d58c9d79af17a482434bf
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-15 16:01:07 -0300

    Committing "Java e java.io" third lesson

---
+++ workspace/java-io/src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
@@ -0,0 +1,39 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+public class TesteEscritaPrintStreamPrintWriter {
+
+	public static void main(String[] args) throws IOException {
+
+		//Fluxo de Saída com Arquivo
+//		OutputStream fos = new FileOutputStream("lorem2.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+		
+//		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+//		PrintStream ps = new PrintStream(new File("lorem2.txt"));
+		PrintWriter ps = new PrintWriter("lorem2.txt");
+		
+		ps.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+		ps.println();
+		ps.println();
+		ps.println();
+		ps.println("asfasdfsafdas dfs sdf asf assdB");
+		
+		ps.close();
+		
+		System.out.println();
+		
+	}
+
+}
