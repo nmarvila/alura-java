d849f7ead5f0a775048b8309bf6d09f5d804dee4
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-18 21:21:47 -0300

    Committing "Java moderno - Java 8" second lesson

--- workspace/Java8/src/OrdenaStrings.java
+++ workspace/Java8/src/OrdenaStrings.java
@@ -10,40 +10,37 @@ public class OrdenaStrings {
 		palavras.add("alura online");
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
-		
-		Comparator<String> comparador = new ComparadorPorTamanho();
-		
-		//Collections.sort(palavras, comparador);
-		palavras.sort(comparador);
-		
-		System.out.println(palavras);
-		
-//		for (String p : palavras) {
-//			System.out.println(p);
-//		}
-		
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
-		
-	}
-}
 
-class ImprimeNaLinha implements Consumer<String> {
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
+		palavras.sort((s1, s2) -> {
+			if (s1.length() < s2.length())
+				return -1;
+			if (s1.length() > s2.length())
+				return 1;
+			return 0;
+		});
+
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		// palavras.sort((s1, s2) -> s1.length() - s2.length());
+
+		System.out.println(palavras);
+
+		Consumer<String> impressor = s -> System.out.println(s);
+		palavras.forEach(impressor);
+
+		palavras.forEach(s -> System.out.println(s));
+
+		new Thread(() -> System.out.println("Executando um Runnable")).start();
+
 	}
 }
 
 class ComparadorPorTamanho implements Comparator<String> {
-
 	@Override
 	public int compare(String s1, String s2) {
-		if(s1.length() < s2.length())
+		if (s1.length() < s2.length())
 			return -1;
-		if(s1.length() > s2.length())
+		if (s1.length() > s2.length())
 			return 1;
 		return 0;
 	}
-	
 }
