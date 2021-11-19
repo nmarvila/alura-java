c3f8d9437db5b820561f986c7088ed2f6d625128
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-19 20:47:38 -0300

    Committing "Java moderno - Java 8" sixth lesson

---
+++ workspace/Java8/src/Datas.java
@@ -0,0 +1,47 @@
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+
+public class Datas {
+	public static void main(String[] args) {
+		LocalDate hoje = LocalDate.now();
+		System.out.println(hoje);
+		
+		LocalDate olimpiadasRio = LocalDate.of(2016, Month.JUNE, 5);
+		
+		int anos = olimpiadasRio.getYear() - hoje.getYear();
+		
+		System.out.println(anos);
+		
+		Period periodo = Period.between(olimpiadasRio, hoje);
+		
+		LocalDate proximasOlimpiadas = olimpiadasRio.plusYears(4);
+		
+		System.out.println(proximasOlimpiadas);
+		
+		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		String valorFormatado = proximasOlimpiadas.format(formatador);
+		System.out.println(valorFormatado);
+		
+		DateTimeFormatter formatadorComHoras = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm");
+
+		LocalDateTime agora = LocalDateTime.now();
+		System.out.println(agora.format(formatadorComHoras));
+		
+		LocalTime intervalo = LocalTime.of(15, 30);
+		System.out.println(intervalo);
+		
+		LocalDate data = LocalDate.of(2099, Month.JANUARY, 25);
+		
+		Period periodo2 = Period.between(hoje, data);
+		
+		System.out.println(agora.format(formatador));
+		
+	}
+}
