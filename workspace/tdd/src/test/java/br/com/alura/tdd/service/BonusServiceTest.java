c9b7ce434c85691af539dd9ae69f48b6a2880da2
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2021-11-22 20:24:51 -0300

    Committing "TDD e Java - JUnit" fourth lesson

--- workspace/tdd/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ workspace/tdd/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,6 +1,8 @@
 package br.com.alura.tdd.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -14,9 +16,16 @@ class BonusServiceTest {
 	@Test
 	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
 		
-		assertEquals(new BigDecimal("0.00"), bonus);
+		//assertThrows(IllegalArgumentException.class,
+			//	() -> service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000"))));
+		
+		try {
+			service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+			fail("Nao deu a exception!");
+		} catch (Exception e) {
+			assertEquals("Funcionario com salario maior do que R$10000 nao pode receber bonus!", e.getMessage());
+		}
 	}
 	
 	@Test
